735acfa44b3d7bc40f90fd1df52df6f65c50d941
Elia Haenggi <elia.haenggi@stud.unibas.ch>
2022-04-25 19:53:26 +0200

    add forwarding table and README (about structure of programms)

--- Controller.py
+++ Controller.py
@@ -4,13 +4,14 @@ import socket
 
 def startController(filepath):
     nameDict, connectionDict = readFile(filepath)
+    sendNames(nameDict)
     sendNTU(nameDict, connectionDict)
 
 
 # Should read the input file and save nodes in nameDict and connections in connectionDict
 def readFile(filepath):
     nameDict = {}  # node names as key, (ip, port) as value
-    connectionDict = {}  # (name, name) as key, number as value
+    connectionDict = {}  # (name, name) as key, cost as value
     f = open(filepath, "r")
     lines = (f.readlines())
     peers = lines[0]
@@ -24,20 +25,11 @@ def readFile(filepath):
     return nameDict, connectionDict
 
 
-# Should first initialize sockets for every peer and afterwards send NTU to every peer. At the end all sockets are
-# closed again
+# Send NTU to every peer. At the end all sockets are closed again
 def sendNTU(nameDict, connectionDict):
-    socketDict = {}  # node names as key, socket as value
-    portOffset = 0  # Offset to have different ports, counted up every loop
-    for peer in nameDict.keys():
-        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clientSocket.bind(("", 8080 + portOffset))  # Bind socket
-        clientSocket.connect(nameDict[peer])
-        socketDict[peer] = clientSocket
-        portOffset += 1
+    socketDict = connectToPeers(nameDict)
     iteration = 0  # Save number of iterations to get last peer to send update information
     for peer in nameDict.keys():
-        message = peer + "|"
         for peerPairs in connectionDict.keys():
             if peerPairs[0] == peer:
                 message = message + peerPairs[1] + "," + nameDict[peerPairs[1]][0] + "," + nameDict[peerPairs[1]][1]
@@ -49,10 +41,37 @@ def sendNTU(nameDict, connectionDict):
         if iteration >= len(nameDict):
             message = message + "-update"
         else:
-            message = message[:-1]  # Cut off last "|" of all not last peers
+            message = message[:-1]  # Cut off last "|" of messages if peer is not last peer
         peerSocket = socketDict[peer]
         peerSocket.send(message.encode())
         peerSocket.close()
+
+
+# Sends list of all peer names. First name is the name of the own peer
+def sendNames(nameDict):
+    socketDict = connectToPeers(nameDict)
+    message = ""
+    for peer in nameDict.keys():
+        for peers in nameDict.keys():
+            if peer != peers:
+                message = message + "|" + peers
+        message = peer + message
+        peerSocket = socketDict[peer]
+        peerSocket.send(message.encode())
+        peerSocket.close()
+
+
+# Connect with sockets to all peers, return Dict with all sockets
+def connectToPeers(nameDict):
+    socketDict = {}  # node names as key, socket as value
+    portOffset = 0  # Offset to have different ports, counted up every loop
+    for peer in nameDict.keys():
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientSocket.bind(("", 8080 + portOffset))  # Bind socket
+        clientSocket.connect(nameDict[peer])
+        socketDict[peer] = clientSocket
+        portOffset += 1
+    return socketDict
 
 
 def main():
--- Peer.py
+++ Peer.py
@@ -2,22 +2,27 @@ import sys
 import socket
 import select
 import time
+import numpy as np
 
 BUFSIZE = 512
 peerName = ""
 connectionDict = {}  # Dictionary of connections, name as key, ((ip, port), number) as value
+peerNameList = []
+global forwardingTable
 
 
 def startPeer(address, port):
+    serverSocket = setupServerSocket(address, port)
     while True:
-        receiveMessage(address, port)
+        receiveMessage(serverSocket)
 
 
 # Used to receive NTU's from the controller and NU's from other peers. New socket is used for every message
-def receiveMessage(address, port):
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.bind((address, port))
+def receiveMessage(serverSocket):
+    global forwardingTable
     serverSocket.listen()
+    receivedNames = False  # If True initialize forwardingTable
+    receivedConnections = False  # If True write connections on diagonal of forwardingTable
     clientSocket, clientAddress = serverSocket.accept()
     sockets_list = [clientSocket]
     while True:
@@ -28,26 +33,50 @@ def receiveMessage(address, port):
                 if not message:
                     print("Connection closed")
                     clientSocket.close()
-                if "|" in message:  # Message from Controller about connection (of the form ...|name,ip,port,number|...
+                if "|" in message:  # Message from Controller about connection (of the form ...|name,ip,port,number|...)
                     peers = message.split("|")
                     for peer in peers:
                         if "-update" in peer:  # -update if peer is last to be informed
-                            # TODO Send NU to all connections
+                            for connection in connectionDict.keys():
+                                message = ""
+                                # TODO send NU to other peers
                             continue
-                        peer = peer.split(",")
-                        if len(peer) == 1:  # First segment is name of peer
-                            peerName = peer
-                        else:
+                        if "," in peer:  # Connection update if message is split with ","
+                            peer = peer.split(",")
                             connectionDict[peer[0]] = ((peer[1], peer[2]), peer[3])  # Update connection
+                            receivedConnections = True
+                        else:
+                            if len(peerNameList) == 0:  # First element of list is own name
+                                peerName = peer
+                            peerNameList.append(peer)  # Name list if no "," in message
+                            receivedNames = True
+                if receivedNames:  # Initialize forwarding table if names received
+                    receivedNames = False
+                    numPeers = len(peerNameList)
+                    forwardingTable = np.matrix(np.ones((numPeers, numPeers)) * np.inf)
+                if receivedConnections:  # Setup numbers for connected peers (should be called after receivedNames)
+                    receivedConnections = False
+                    iterations = 0
+                    for peer in peerNameList:
+                        if peer in connectionDict.keys():
+                            forwardingTable[iterations][iterations] = connectionDict[peer][1]
+                        iterations += 1
+                # TODO If received NU from other peer update forwarding-table and send NU to connections
                 clientSocket.close()
                 return
         time.sleep(0.01)
 
 
+def setupServerSocket(address, port):
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.bind((address, port))
+    return serverSocket
+
+
 def main():
     if len(sys.argv) == 3:
         address = sys.argv[1]
-        port = sys.argv[2]
+        port = int(sys.argv[2])
         startPeer(address, port)
     else:
         print("Error, wrong arguments")
